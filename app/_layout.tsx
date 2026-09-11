f42c77dfd5ebc502579b86d71432e0330bbb009a
fverzosa147371 <f.verzosa.147371.tc@umindanao.edu.ph>
2026-09-11 20:13:42 +0800

    exporoutesstudentportal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,6 @@
 import { Stack } from "expo-router";
-import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
